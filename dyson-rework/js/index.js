ea0716900af49df36d0879c607f79b9f2cbb7ecf
Nikita <nik.sedov890@gmail.com>
2026-07-26 01:24:39 +0300

    Add files via upload

---
+++ dyson-rework/js/index.js
@@ -0,0 +1,25 @@
+import BurgerMenu from "./burger.js";
+
+try {
+  // const headerFixed = new HeaderFixed({
+  //    HEADER: "header",
+  //    HEADER_FIXED: "header--fixed",
+  // });
+
+  new BurgerMenu({
+    BURGER: "burger",
+    BURGER_OPEN: "burger--open",
+    HEADER_MENU: "header__menu",
+    HEADER_MENU_OPEN: "header__menu--open",
+    LABEL: {
+      OPEN: "Открыть меню",
+      CLOSE: "Закрыть меню",
+    },
+    PAGE_BODY: "page__body",
+    PAGE_BODY_NO_SCROLL: "page__body--no-scroll",
+    MENU_LINK: "header__nav-link",
+    BREAKPOINT: 1200,
+  });
+} catch (error) {
+  console.error(error);
+}
